c598822f26904612030e1c592367d97ca7d9d9a1
KsquaredK <ksquaredkelly@gmail.com>
2021-06-13 16:50:13 -0400

    animalSearch -w.i.p.

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -35,19 +35,21 @@ export const AnimalDetail = () => {
   // JSX for rendering individual animal details
 
   return (
-    <section className="animal">
-      <button
-        onClick={() => {
-          history.push(`/animals/edit/${animal.id}`);
-        }}>
-        Edit your pet's info
-      </button>
-      <p></p>
-      <button onClick={handleRelease}>Release Animal</button>
-      <h3 className="animal__name">{animal.name}</h3>
-      <div className="animal__breed">{animal.breed}</div>
-      <div className="animal__location">Location: {animal.location.name}</div>
-      <div className="animal__owner">Customer: {animal.customer.name}</div>
-    </section>
+    <>
+      <section className="animal">
+        <button
+          onClick={() => {
+            history.push(`/animals/edit/${animal.id}`);
+          }}>
+          Edit your pet's info
+        </button>
+        <p></p>
+        <button onClick={handleRelease}>Release Animal</button>
+        <h3 className="animal__name">{animal.name}</h3>
+        <div className="animal__breed">{animal.breed}</div>
+        <div className="animal__location">Location: {animal.location.name}</div>
+        <div className="animal__owner">Customer: {animal.customer.name}</div>
+      </section>
+    </>
   );
 };
